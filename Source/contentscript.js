c0d782b12aa2cf0036cfe4cab7fd960b9d0af367
sahil <sahil@slavingia.com>
2013-07-28 19:45:54 -0700

    cleaned up js

--- Source/contentscript.js
+++ Source/contentscript.js
@@ -1,12 +1,8 @@
-var allImages = $('img');
-var largeImages = allImages.filter(function(){
-	return ($(this).width() > 100) & ($(this).height() > 100)
-})
+$('img').filter(function() {
+  return $(this).width() > 100 && $(this).height() > 100;
+}).each(function(image) {
+  var splitURL = image.src.split("/")
+    , downloadURL = splitURL[0] + "//" + splitURL[2] + "/" + splitURL[3] + "/" + splitURL[5] + "?dl=1";
 
-for (var i = 0; i < largeImages.length; i++) {
-	var themURL = (largeImages[i].src);
-	var splitURL = themURL.split("/");
-	var newURL = splitURL[0] + "//" + splitURL[2] + "/" + splitURL[3] + "/" + splitURL[5];
-	var downloadLink = newURL + "?dl=1";
-	window.open(downloadLink, '_blank');
-}
+  window.open(downloadURL, '_blank');
+});
